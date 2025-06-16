8ae0b19383afdc670a7b15bb82cdee28352731f2
Mehak Noorani <mehak@miletap.com>
2025-06-16 12:54:46 +0500

    Initial Commit

---
+++ coder-swagTests/coder_swagTests.swift
@@ -0,0 +1,17 @@
+//
+//  coder_swagTests.swift
+//  coder-swagTests
+//
+//  Created by Workwise 1 on 16/06/2025.
+//
+
+import Testing
+@testable import coder_swag
+
+struct coder_swagTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
